c0a7662cb3db5a9c8cf43f9b5e254042008d4734
Abhinand <abhinand2p@gmail.com>
2023-08-04 14:44:33 +0530

    final commit

--- chatgptAPI/script.js
+++ chatgptAPI/script.js
@@ -1,14 +1,24 @@
 import { config } from "dotenv"
 import { Configuration, OpenAIApi } from "openai"
+import readline from "readline"
 config()
 
 const openai = new OpenAIApi(new Configuration({
     apiKey : process.env.API_KEY
 }))
 
-openai.createChatCompletion({
-    model : "gpt-3.5-turbo",
-    messages : [{role : "user", content : "Hello Chat"}]
-}).then(res => {
-    console.log(res);
+const UserInterface = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
 })
+
+UserInterface.prompt()
+UserInterface.on("line", async input => {
+    const res = await openai.createChatCompletion({
+        model : "gpt-3.5-turbo",
+        messages : [{role : "user", content : input}]
+    })
+    console.log(res.data.choices[0].message.content);
+    UserInterface.prompt()
+})
+
